180768ef979011460d395b2fb0b2ad255b66bafc
jozkan30 <jozkan2@gmail.com>
2023-04-19 13:14:07 -0400

    auth working

--- seed.js
+++ seed.js
@@ -1,5 +1,7 @@
 import connection from './connection.js'
 import Review from './models/Wine_model.js'
+import User from "./models/User.js";
+import bcrypt from "bcrypt"
 
 const reviews = [
     {
@@ -16,6 +18,15 @@ const reviews = [
     const insertData = async () => {
         await connection.dropDatabase();
         await Review.create(reviews)
+        const user1 = new User({
+        username: 'justin',
+        email: 'someone@gmail.com',
+        password: await bcrypt.hash('1234', 11)
+      })
+    await user1.save()
+    console.log('Created users');
+
+
         await connection.close();
     }
     
